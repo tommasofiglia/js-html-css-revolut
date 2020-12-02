6d03aef3a99f4ea8fa2aec130a7681fbbe528edc
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-02 19:27:43 +0100

    Esercizio "Versione corretta" completato

--- Versione corretta/assets/js/main.js
+++ Versione corretta/assets/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
 
+  var menuDropdown = $('.dropdown_menu');
+  var links = $('.right_navbar > ul > li');
+
+  links.mouseenter(function() {
+    $(this).children('.dropdown_menu').removeClass('hidden');
+  });
+
+  links.mouseleave(function() {
+    $(this).children('.dropdown_menu').addClass('hidden');
+  });
+
+
 });
+
+next()
